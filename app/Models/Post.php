eff09cb2fb08441910bc84d81f1d38bcff0ce87c
Rubrasum <josephbetbeze@gmail.com>
2023-04-22 22:56:10 -0500

    Added mass assignment options to Post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+//    protected $fillable = ['title', 'excerpt', 'body']; // allows only these explicit fields to be mass assigned
+//    protected $guarded = ['id']; // disables these fields from being mass assigned
+    protected $guarded = []; // allows all mass assigned but can be used to
 }
