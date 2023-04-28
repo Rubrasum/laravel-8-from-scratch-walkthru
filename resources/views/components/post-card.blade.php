c9c6c35defd269cd8c2d2b1e1d84a9b209b070ac
josephbetbeze123 <josephbetbeze@gmail.com>
2023-04-28 10:31:10 -0500

    Setup post-card.blade.php to use the post prop and add  in the class name, setup posts page to display posts.

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -1,7 +1,7 @@
-
+@props(['post'])
 
 <article
-    class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
+        {{ $attributes->merge(['class' => "transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl"]) }} >
     <div class="py-6 px-5">
         <div>
             <img src="./images/illustration-1.png" alt="Blog Post illustration" class="rounded-xl">
@@ -12,7 +12,7 @@
                 <div class="space-x-2">
                     <a href="/categories/{{ $post->category->slug }}"
                        class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
-                       style="font-size: 10px">{ $post->category->name }</a>
+                       style="font-size: 10px">{{ $post->category->name }}</a>
                 </div>
 
                 <div class="mt-4">
