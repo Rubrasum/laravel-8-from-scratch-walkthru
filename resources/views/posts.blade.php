e2cc27043eec3189bd87bc00d74f24e8564257b4
josephbetbeze123 <josephbetbeze@gmail.com>
2023-04-28 11:02:57 -0500

    Removed extra comments

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -10,23 +10,4 @@
             <p class="text-center">No posts yet! Try back later.</p>
         @endif
     </main>
-
-
-{{--    @foreach ($posts as $post)--}}
-{{--        <article>--}}
-{{--            <h2>--}}
-{{--                <a href="/posts/{{$post->slug}}">--}}
-{{--                    {{ $post->title }}--}}
-{{--                </a>--}}
-{{--            </h2>--}}
-
-{{--            <p>--}}
-{{--                By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>--}}
-{{--            </p>--}}
-
-{{--            <div>--}}
-{{--                {{ $post->excerpt }}...--}}
-{{--            </div>--}}
-{{--        </article>--}}
-{{--    @endforeach--}}
 @endsection
