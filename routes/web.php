79090d90877bfa5c9342aedda04a6b81e1e98321
josephbetbeze123 <josephbetbeze@gmail.com>
2023-04-28 20:15:37 -0500

    Adding components for individual things, adding a bunch of frontend methods and functions for that dropdown functionality, naming routes.

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
         'posts' => Post::latest()->with('category', 'author')->get(),
         'categories' => Category::all()
     ]);
-});
+})->name('home');
 
 Route::get('posts/{post:slug}', function (Post $post) { // Post::where('slug', $post)->firstOrFail();
 
@@ -39,7 +39,7 @@ Route::get('categories/{category:slug}', function (Category $category) { // Post
         'currentCategory' => $category,
         'categories' => Category::all()
     ]);
-});
+})->name('category');
 
 Route::get('authors/{author:username}', function (User $author) { // Post::where('slug', $post)->firstOrFail();
 
