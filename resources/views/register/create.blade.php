cd90437937c414d93590ed91222d990159ba5e0d
josephbetbeze123 <josephbetbeze@gmail.com>
2023-05-01 13:28:31 -0500

    Added error directives, set default values with old, added unique rule to RegisterController.php, created a visual for errors on create.blade.php, commented out the foreach version. #47

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -19,8 +19,12 @@
                            type="text"
                            name="name"
                            id="name"
+                           value="{{ old('name') }}"
                            required
                     >
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -34,8 +38,13 @@
                            type="text"
                            name="username"
                            id="username"
+                           value="{{ old('username') }}"
                            required
                     >
+
+                    @error('username')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
@@ -48,11 +57,12 @@
                            type="email"
                            name="email"
                            id="email"
+                           value="{{ old('email') }}"
                            required
                     >
 
-                    @error('')
-                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                    @error('email')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                     @enderror
                 </div>
                 <div class="mb-6">
@@ -68,6 +78,10 @@
                            id="password"
                            required
                     >
+
+                    @error('password')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="mb-6">
                     <button type="submit"
@@ -76,6 +90,15 @@
                         Submit
                     </button>
                 </div>
+
+{{--                @if ($errors->any())--}}
+{{--                    <ul>--}}
+{{--                        @foreach($errors->all() as $error )--}}
+{{--                            <li class="text-ref-500 text-xs">{{ $error }}</li>--}}
+{{--                        @endforeach--}}
+{{--                    </ul>--}}
+{{--                @endif--}}
+
             </form>
         </main>
     </section>
