6081aba1e89fe1d2204185e59fd99a7b2b6665c4
Rubrasum <josephbetbeze@gmail.com>
2023-05-02 14:42:32 -0500

    Added a submit button component. #57

--- resources/views/posts/_add-comment-form.blade.php
+++ resources/views/posts/_add-comment-form.blade.php
@@ -20,13 +20,7 @@
                 <span class="text-xs text-red-500">{{ $message }}</span>
                 @enderror
             </div>
-
-            <div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
-                <button
-                    class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600"
-                    type="submit">Post</button>
-            </div>
-
+            <x-submit-button>Post</x-submit-button>
         </form>
     </x-panel>
 @else
