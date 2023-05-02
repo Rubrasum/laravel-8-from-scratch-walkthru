7ec1b9e2c2c8453861c3837979162efeaf39010d
Rubrasum <josephbetbeze@gmail.com>
2023-05-01 21:31:32 -0500

    Setup all the Category and Comment and Post and User factories finally.

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -2,21 +2,20 @@
 
 namespace Database\Factories;
 
-use App\Models\Comment;
-use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
  */
-class CommentFactory extends Factory
+class CategoryFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
      *
      * @var string
      */
-    protected $model = Comment::class;
+    protected $model = Category::class;
 
     /**
      * Define the model's default state.
@@ -26,10 +25,8 @@ class CommentFactory extends Factory
     public function definition(): array
     {
         return [
-            'post_id' => Post::factory(),
-            'user_id' => Post::factory(),
-            'body' => $this->faker->paragraph()
-            //
+            'name' => $this->faker->unique()->word(),
+            'slug' => $this->faker->unique()->slug()
         ];
     }
 }
