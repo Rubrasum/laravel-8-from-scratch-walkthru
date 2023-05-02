ef5d30e6da09e5bba86f5093d35581127f69946b
Rubrasum <josephbetbeze@gmail.com>
2023-05-01 21:46:14 -0500

    Created comment post box/form.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -54,6 +54,31 @@
                 </div>
 
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
+                    <form method="POST" action="#" class="border border-gray-200 p-6 rounded-xl">
+                        @csrf
+
+                        <header class="flex items-center">
+                            <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}" alt="" width="60" height="50" class="rounded-full">
+                            <h2 class="ml-4">Want to participate?</h2>
+                        </header>
+
+                        <div class="mt-6">
+                            <textarea
+                                name="body"
+                                cols="30"
+                                rows="10"
+                                class="w-full text-sm focus:outline-none focus:ring"
+                                placeholder="Quick think of something to say"></textarea>
+                        </div>
+
+                        <div class="flex justify-end">
+                            <button
+                                class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600"
+                                type="submit">Post</button>
+                        </div>
+
+                    </form>
+
                     @foreach ($post->comments as $comment)
                         <x-post-comment :comment="$comment"/>
                     @endforeach
