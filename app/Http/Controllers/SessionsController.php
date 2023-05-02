e38ba522260c10fe49a8758929b5dd5042b1e92c
Rubrasum <josephbetbeze@gmail.com>
2023-05-01 20:44:46 -0500

    Creating login page, added route in routes file, added function in SessionsController.php, added view create.blade.php in views/sessions (copied from registration page and using normal layout), adding post route for login with validation and auth check and success message #50

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class SessionsController extends Controller
 {
@@ -10,5 +11,28 @@ class SessionsController extends Controller
         auth()->logout();
 
         return redirect('/')->with('success', "Goodbye!");
+    }
+
+    public function create() {
+        return view('sessions.create');
+    }
+
+    // Attempt to authenticate and log in the user
+    // based on the provided credentials
+    public function store() {
+        $attributes = request()->validate([
+            'email' => ['required','email'],
+            'password' => 'required'
+        ]);
+
+        // signs you in and checks
+        if (auth()->attempt($attributes)) {
+            return redirect('/')->with('success', 'Welcome Back!'); // redirect, success flash
+        }
+
+        // auth failed.
+        throw ValidationException::withMessages([
+            'email' => 'Your provided credentials could not be verified'
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,9 @@ Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
 // opposite of guest is auth, you can check it out at app/Http/Kernel.php
-Route::post('logout', [SessionsController::class, 'destroy']);
+Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
+Route::post('sessions', [SessionsController::class, 'store'])->middleware('guest');
+Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
 
 
 
