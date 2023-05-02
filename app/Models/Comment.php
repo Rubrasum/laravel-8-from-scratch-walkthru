99ad3451733dae545198c47772b680ce6fcbdd81
Rubrasum <josephbetbeze@gmail.com>
2023-05-01 21:34:31 -0500

    Updated Comment and Post with relationship

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    public function post() { // post_id
+        return $this->belongsTo(Post::class);
+    }
+
+    public function author() { // author_id - requires override
+        // have to be specific because it's called author in table
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
