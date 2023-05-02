76378c40fe78cf5560cbce0932d7e18afe44dd85
Rubrasum <josephbetbeze@gmail.com>
2023-05-01 21:40:39 -0500

    Made comments fully dynamic #54

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -27,7 +27,7 @@ class CommentFactory extends Factory
     {
         return [
             'post_id' => Post::factory(),
-            'user_id' => Post::factory(),
+            'user_id' => User::factory(),
             'body' => $this->faker->paragraph()
             //
         ];
